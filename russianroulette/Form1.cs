051353fa02063fc505dab9c496933cec961671d9
Rupinder Singh <RS044@atcltd.atc.org.nz>
2018-07-24 14:42:42 +1200

    Final Version

---
+++ russianroulette/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace russianroulette
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Game game = new Game();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            game.start();
+            button2.Enabled = false;
+            label2.Text = "Gun Loaded and Spinned.";
+            label2.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string fire = game.fire();
+            if(fire == "killed")
+            {
+                label2.Text = "You are killed.";
+                label2.Visible = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            game.fireAway();
+        }
+    }
+}
---
+++ russianroulette/game.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace russianroulette
+{
+    class Game
+    {
+        public int[] gunBarrel = { 0, 0, 0, 0, 0, 0 };
+        int chances = 2;
+        public void start()
+        {
+            Random rand = new Random();
+            int val = rand.Next(0, 5);
+            gunBarrel[val] = 1;
+            Console.WriteLine("" + val);
+            loadBarrel();
+        }
+
+        public void loadBarrel()
+        {
+            SoundPlayer player = new SoundPlayer("loading_and_spinning.wav");
+            player.Play();
+        }
+
+        public string fire()
+        {
+            if (gunBarrel[0] == 1)
+            {
+                SoundPlayer player = new SoundPlayer("gun_shot.wav");
+                player.Play();
+                return "killed";
+            }
+            else
+            {
+                SoundPlayer player = new SoundPlayer("gun_cock.wav");
+                player.Play();
+                gunBarrel = gunBarrel.Skip(1).ToArray();
+                return "safe";
+            }
+        }
+
+        public string fireAway()
+        {
+            chances--;
+            if (chances == 0)
+            {
+                if (gunBarrel[0] == 1)
+                {
+                    return "bullet gone";
+                }
+                else
+                {
+                    gunBarrel = gunBarrel.Skip(1).ToArray();
+                    return "nothing";
+                }
+            }
+            else
+            {
+                if (gunBarrel[0] == 1)
+                {
+                    return "You win";
+                }
+                else
+                {
+                    return "nothing";
+                }
+            }
+        }
+    }
+}
